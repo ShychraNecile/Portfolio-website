776b67522b36b8cac4d5c50f486b419ae7a532ce
Annemarleen <amholbosma@gmail.com>
2021-02-16 16:43:10 +0100

    Created first setup for personal portfolio website

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
-    
+    'projects',    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["personal_portfolio/templates/"],
+        'DIRS': ['personal_portfolio/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
